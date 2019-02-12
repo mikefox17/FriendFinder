55deb95a05eea46b93ce1bf0f7f493dc1373860d
Michael Fox <mikefox17@gmail.com>
2019-02-12 18:53:29 -0500

    heroku time

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+  {
+    name: "Aileen",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Wuornos.jpg/220px-Wuornos.jpg",
+    scores: [5, 3, 1, 1, 4, 4, 2, 3, 2, 1]
+  },
+  {
+    name: "Bill",
+    photo: "https://www.famousbirthdays.com/headshots/bill-murray-8.jpg",
+    scores: [3, 3, 4, 5, 2, 2, 3, 2, 1, 4]
+  },
+  {
+    name: "Catherine Beta Jones",
+    photo:
+      "https://deepbluebettas.com/wp-content/uploads/2018/10/Siamese-fighting-300x300.png",
+    scores: [4, 4, 4, 2, 2, 1, 3, 5, 5, 2]
+  },
+  {
+    name: "Nic",
+    photo:
+      "http://www.wow247.co.uk/wp-content/uploads/2014/06/Nicolas-Cage-you-dont-say.jpg",
+    scores: [5, 3, 1, 1, 4, 4, 2, 3, 2, 1]
+  },
+  {
+    name: "Dog",
+    photo:
+      "http://gooddaydoodles.com/wp-content/uploads/2018/12/ellie-headshot.jpg",
+    scores: [1, 1, 1, 5, 5, 5, 2, 4, 2, 4]
+  },
+  {
+    name: "Jeff",
+    photo:
+      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNTk5NzU4ODYx/jeffrey-dahmer-9264755-1-402.jpg",
+    scores: [4, 5, 3, 4, 4, 5, 3, 2, 1, 4]
+  },
+  {
+    name: "Cat",
+    photo:
+      "https://image.shutterstock.com/image-photo/closeup-maine-coon-looking-camera-260nw-635622530.jpg",
+    scores: [2, 4, 3, 1, 5, 5, 2, 4, 1, 4]
+  }
+];
+
+module.exports = friends;
